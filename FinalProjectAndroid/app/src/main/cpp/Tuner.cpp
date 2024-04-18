e79ee4daaabe4e1f91c9ad75a9119da581aee125
Joel <joelbs3@illinois.edu>
2024-04-18 17:04:22 -0500

    Refactored TDPSOLA Code into Tuner class

---
+++ FinalProjectAndroid/app/src/main/cpp/Tuner.cpp
@@ -0,0 +1,5 @@
+//
+// Created by joels on 4/18/2024.
+//
+
+#include "Tuner.h"
---
+++ FinalProjectAndroid/app/src/main/cpp/Tuner.h
@@ -0,0 +1,16 @@
+//
+// Created by joels on 4/18/2024.
+//
+
+#ifndef FINALPROJECTANDROID_TUNER_H
+#define FINALPROJECTANDROID_TUNER_H
+
+
+
+class Tuner {
+
+};
+
+
+
+#endif //FINALPROJECTANDROID_TUNER_H
