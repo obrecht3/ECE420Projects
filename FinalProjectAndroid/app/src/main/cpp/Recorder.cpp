dd76ab9657ef51206eebdd370ad74d929745a1c8
Joel <joelbs3@illinois.edu>
2024-04-29 22:43:07 -0500

    Recorder as good as it'll get for now

--- FinalProjectAndroid/app/src/main/cpp/Recorder.cpp
+++ FinalProjectAndroid/app/src/main/cpp/Recorder.cpp
@@ -1,14 +1,16 @@
 #include "Recorder.h"
 #include "Filter.h"
+#include "Tuner.h"
 
-Recorder::Recorder(int _sampleRate, int _frameSize, double maxRecordLength)
+Recorder::Recorder(int _sampleRate, int _frameSize, double _maxRecordLength)
     : sampleRate{_sampleRate}
     , frameSize{_frameSize}
+    , maxRecordLength{_maxRecordLength}
     , pos{0}
     , recording{false}
     , playing{false}
     , fadeLength{24}
-    , signal(static_cast<int>(sampleRate * maxRecordLength), 0.0f)
+    , signal(static_cast<size_t>(sampleRate * maxRecordLength), 0.0f)
     , nextBuffer(frameSize){
 
 }
@@ -20,7 +22,10 @@ Recorder::~Recorder() {
 void Recorder::start() {
     pos = 0;
     recording = true;
-    signal.resize(signal.size(), 0.0f);
+    signal.resize(static_cast<size_t>(sampleRate * maxRecordLength));
+    for (size_t i = 0; i < signal.size(); ++i) {
+        signal[i] = 0.0f;
+    }
 }
 
 void Recorder::stop() {
@@ -33,30 +38,17 @@ void Recorder::stop() {
     if (recording) {
         recording = false;
 
-        // process signal
+        size_t startSample = findStartSample();
+        size_t endSample = findEndSample();
 
-        // find envelope
-        std::vector<float> envelope(signal.size());
-        std::vector<SinglePoleLPF> filter(4, SinglePoleLPF(sampleRate));
-        for (auto f : filter) {
-            f.setG(f.calcG(5));
-        }
-
-        float magnitude = 0.0f;
-        for (int i = 0; i < signal.size(); ++i) {
-            float sample = std::abs(signal[i]);
-            for (auto f : filter) {
-                sample = f.processSample(sample);
+        if (endSample - startSample > 0) {
+            for (size_t i = startSample; i < endSample; ++i) {
+                signal[i - startSample] = signal[i];
             }
-            envelope[i] = sample;
-            magnitude = std::max(magnitude, sample);
-        }
 
-//        // find start and end of note
-//        int startSample = 0;
-//        for (int i = 0; i < signal.size(); ++i) {
-//
-//        }
+            signal.resize(endSample - startSample);
+            applyFades();
+        }
     }
 }
 
@@ -67,18 +59,13 @@ void Recorder::play() {
 }
 
 void Recorder::writeData(float *data) {
-    const int fadeOutSample = signal.size() - fadeLength;
     for (int i = 0; i < frameSize; ++i) {
         if (pos >= signal.size()) {
             stop();
             return;
-        } else if (pos <= fadeLength) {
-            signal[pos] = data[i] * static_cast<float>(pos) / static_cast<float>(fadeLength);
-        } else if (pos >= fadeOutSample) {
-            signal[pos] = data[i] * (1.0f - static_cast<float>(pos - fadeOutSample) / static_cast<float>(fadeLength));
-        } else {
-            signal[pos] = data[i];
         }
+
+        signal[pos] = data[i];
         ++pos;
     }
 }
@@ -87,12 +74,69 @@ float *Recorder::getNextBuffer() {
     if (!playing) return nextBuffer.data();
 
     for (int i = 0; i < frameSize; ++i) {
+        nextBuffer[i] = signal[pos];
+        ++pos;
         if (pos >= signal.size()) {
-            nextBuffer[i] = 0.0f;
-        } else {
-            nextBuffer[i] = signal[pos];
-            ++pos;
+            pos = 0;
         }
     }
     return nextBuffer.data();
+}
+
+size_t Recorder::findStartSample() {
+    Tuner periodDetector(frameSize, sampleRate, 0);
+
+    const size_t numFrames = signal.size() / frameSize;
+    float data[frameSize];
+    size_t signalPos = 0;
+
+    for (size_t frame = 0; frame < numFrames; ++frame) {
+        for (size_t i = 0; i < frameSize; ++i) {
+            data[i] = signal[signalPos];
+            signalPos++;
+        }
+
+        periodDetector.writeInputSamples(data);
+        int period = periodDetector.detectBufferPeriod();
+
+        if (period > 50) {
+            return frame * frameSize;
+        }
+    }
+
+    return 0;
+}
+
+size_t Recorder::findEndSample() {
+    Tuner periodDetector(frameSize, sampleRate, 0);
+
+    const size_t numFrames = signal.size() / frameSize;
+    float data[frameSize];
+    size_t signalPos = frameSize * numFrames;
+
+    for (int frame = numFrames - 1; frame >= 0; --frame) {
+        for (size_t i = 0; i < frameSize; ++i) {
+            data[i] = signal[signalPos];
+            signalPos--;
+        }
+
+        periodDetector.writeInputSamples(data);
+        int period = periodDetector.detectBufferPeriod();
+
+        if (period > 50) {
+            return frame * frameSize;
+        }
+    }
+
+    return 0;
+}
+
+void Recorder::applyFades() {
+    for (size_t i = 0; i < fadeLength; ++i) {
+        signal[pos] *= static_cast<float>(pos) / static_cast<float>(fadeLength);
+    }
+
+    for (size_t i = signal.size() - fadeLength; i < signal.size(); ++i) {
+        signal[pos] *= (1.0f - static_cast<float>(pos) / static_cast<float>(fadeLength));
+    }
 }
--- FinalProjectAndroid/app/src/main/cpp/Recorder.h
+++ FinalProjectAndroid/app/src/main/cpp/Recorder.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <math.h>
 
 class Recorder {
 public:
@@ -17,13 +18,23 @@ public:
     float* getNextBuffer();
 
 private:
+    size_t findStartSample();
+    size_t findEndSample();
+
+    void applyFades();
+
+private:
     int sampleRate;
     int frameSize;
+    double maxRecordLength;
     size_t pos;
     bool recording;
     bool playing;
 
     const int fadeLength;
+
+//    float attack;
+//    float release;
 
     std::vector<float> signal;
     std::vector<float> nextBuffer;
